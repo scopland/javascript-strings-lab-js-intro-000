a4fcc27c5ec062f710ccf57da10c0c959844e86e
Samantha Copland <sam.copland1992@gmail.com>
2020-05-05 20:01:28 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,7 +3,8 @@ var greeting = "Hello, everybody!";
 
 var specialGuest = "Neil deGrasse Tyson"
 
-var greetSpecialGuest = "" + specialGuest + "!";
+var greetSpecialGuest = "Hello" + specialGuest + "!";
+
 
 var topic = "space";
 
